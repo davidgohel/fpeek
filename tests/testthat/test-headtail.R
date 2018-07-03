6a6c805068d1724e5637a9d876e4ba8296e77072
davidgohel <david.gohel@ardata.fr>
2018-07-03 16:50:44 +0200

    test

--- tests/testthat/test-headtail.R
+++ tests/testthat/test-headtail.R
@@ -1,18 +1,20 @@
 context("head tail tests")
 
 file <- system.file(package = "fpeek", "datafiles", "cigale-ISO-8859-1.txt")
+file_utf8 <- tempfile()
+file_iconv_write(path = file, newfile = file_utf8, from = "WINDOWS-1252", to = "UTF-8")
 
 test_that("ckeck head output", {
 
-  expect_output(file_head_show(file, n = 1), regexp = "La Cigale et la Fourmi")
-  expect_equal(file_head_char(file, n = 1),  "La Cigale et la Fourmi")
+  expect_output(file_head_show(file_utf8, n = 1), regexp = "La Cigale et la Fourmi")
+  expect_equal(file_head_char(file_utf8, n = 1),  "La Cigale et la Fourmi")
 
 })
 
 test_that("ckeck tail output", {
 
-  expect_output(file_tail_show(file, n = 1), regexp = "Eh bien! dansez maintenant.")
-  expect_equal(file_tail_char(file, n = 1),  "Eh bien! dansez maintenant.")
+  expect_output(file_tail_show(file_utf8, n = 1), regexp = "Eh bien! dansez maintenant.")
+  expect_equal(file_tail_char(file_utf8, n = 1),  "Eh bien! dansez maintenant.")
 
 })
 
